36cbbffa04638c3d42dbff6ff34e5cc8ccb52a7e
andy <andy.bartkiv@gmail.com>
2021-02-15 19:06:28 +0300

    Final Release

--- main.js
+++ main.js
@@ -8,17 +8,19 @@ function clearGrid() {
 
 function drawGrid() {
     let gridSize = document.querySelector('#grid-size').value;
-    const del = [...document.querySelectorAll('.div-rows')];
+    gridContainer.setAttribute('style', `    grid-template-columns: repeat(${gridSize}, 1fr);
+    grid-template-rows: repeat(${gridSize}, 1fr);`)
+    const del = [...document.querySelectorAll('.div-box')];
     del.forEach(e => e.remove());
     for (let i=0; i<gridSize; i++) {
-        const divRows = document.createElement('div');
-        gridContainer.appendChild(divRows);
-        divRows.setAttribute('class', 'div-rows');
+    //    const divRows = document.createElement('div');
+    //    gridContainer.appendChild(divRows);
+    //    divRows.setAttribute('class', 'div-rows');
         for (let j=0; j<gridSize; j++){
             const divBox = document.createElement('div');
             divBox.setAttribute('class', 'div-box');
             divBox.setAttribute('id', `box-${i}-${j}`);
-            divRows.appendChild(divBox);
+            gridContainer.appendChild(divBox);
             divBox.addEventListener('mouseover', () =>{
                 divBox.classList.add('m-over');
                 divBox.setAttribute('style', `background: ${color.value}`)
@@ -32,9 +34,11 @@ color.value = '#0000ff';
 
 const actualSize = document.querySelector('#grid-size');
 actualSize.value = 24;
-document.querySelector('#lbl-size').textContent = actualSize.value;
+document.querySelectorAll('.gsv').forEach(el =>
+    el.textContent = actualSize.value)
 actualSize.addEventListener('mousemove', () => {
-    document.querySelector('#lbl-size').textContent = actualSize.value;
+    document.querySelectorAll('.gsv').forEach(el =>
+        el.textContent = actualSize.value)
 })
 
 const btnClear = document.querySelector('#clear-btn');
